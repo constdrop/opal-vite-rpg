c8024d0a4ec42320c8193b39a528a67df26f5534
constdrop <iota_tz@yahoo.co.jp>
2026-02-13 22:27:36 +0900

    step/03: Update index.html and main.rb for lesson step 3

--- app/main.rb
+++ app/main.rb
@@ -3,13 +3,10 @@ require 'opal'
 require 'native'
 
 # 1. HTMLの要素を見つける
-message_div = `document.getElementById('message-area')`
+log_area = `document.getElementById('log-area')`
 button = `document.getElementById('action-button')`
 player_name_input = `document.getElementById('player-name')`
 enemy_hp_span = `document.getElementById('enemy-hp')`
-
-# ゲームのデータ
-@enemy_hp = 30
 
 # 2. ボタンが押された時の処理（イベント）
 `#{button}.addEventListener('click', function() {`
@@ -19,26 +16,53 @@ enemy_hp_span = `document.getElementById('enemy-hp')`
 
   # 判断1：名前が入っているか？
   if player_name == ""
-    `#{message_div}.innerText = "名前を入力してください！"`
+    `#{log_area}.innerHTML = "名前を入力してください！"`
   else
-    # --- 名前がある時だけ、以下のバトル処理を実行する ---
+    # バトルの初期設定（インスタンス変数 @ を使って値を保持する）
+    `#{log_area}.innerHTML = "--- 冒険スタート！ ---<br>"`
+    `#{button}.disabled = true`
     
-    damage = 10
-    @enemy_hp -= damage
-    
-    # HPがマイナスにならないようにする（第2回のポイント！）
-    @enemy_hp = 0 if @enemy_hp < 0
-    `#{enemy_hp_span}.innerText = #{@enemy_hp}`
+    @enemy_count = 1
+    @enemy_hp = 30
 
-    # 判断2：敵を倒したか？
-    if @enemy_hp <= 0
-      message = "#{player_name}の攻撃！スライムをたおした！"
-      `#{button}.disabled = true`
-    else
-      message = "#{player_name}の攻撃！スライムに #{damage} のダメージ！"
-    end
-    
-    `#{message_div}.innerText = #{message}`
+    # ★ 0.5秒ごとに実行するタイマー
+    timer = `setInterval(function() {`
+      
+      # 攻撃のダメージ計算
+      damage = rand(5..15)
+      @enemy_hp -= damage
+      @enemy_hp = 0 if @enemy_hp < 0
+      
+      # 画面を更新（一度Rubyでメッセージを作ってから渡す！）
+      `#{enemy_hp_span}.innerText = #{@enemy_hp}`
+      
+      msg = "#{player_name}の攻撃！#{damage}のダメージ！(残りHP:#{@enemy_hp})<br>"
+      `#{log_area}.innerHTML += #{msg}`
+      `#{log_area}.scrollTop = #{log_area}.scrollHeight`
+
+      # 判断2：敵を倒したか？
+      if @enemy_hp <= 0
+        finish_msg = "<b>スライム#{@enemy_count}をたおした！</b><br>"
+        `#{log_area}.innerHTML += #{finish_msg}`
+        
+        if @enemy_count < 3
+          # まだ次の敵がいる場合
+          @enemy_count += 1
+          @enemy_hp = 30 + (@enemy_count * 10)
+          next_msg = ">> 次の敵があらわれた！<br>"
+          `#{log_area}.innerHTML += #{next_msg}`
+        else
+          # すべて倒した場合：ループを止める
+          `clearInterval(#{timer})`
+          complete_msg = "<b>★すべての敵を撃破した！★</b>"
+          `#{log_area}.innerHTML += #{complete_msg}`
+          `#{log_area}.scrollTop = #{log_area}.scrollHeight`
+          `#{button}.disabled = false`
+        end
+      end
+
+    `}, 500)` # 500ミリ秒ごとに繰り返す
+
   end
   
 `})`
