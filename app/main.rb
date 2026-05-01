6cd23002d4b7dd31ec678d7efee91a204318bc4c
constdrop <iota_tz@yahoo.co.jp>
2026-05-01 22:09:30 +0900

    Update index.html and main.rb for lesson step 7

--- app/main.rb
+++ app/main.rb
@@ -6,18 +6,31 @@ require 'native'
 
 # プレイヤーの設計図
 class Player
-  attr_accessor :name
+  attr_accessor :name, :hp, :max_hp
   def initialize(name)
     @name = name
+    @max_hp = 50
+    @hp = @max_hp
+  end
+
+  def receive_damage(amount)
+    @hp -= amount
+    @hp = 0 if @hp < 0
+  end
+
+  def heal(amount)
+    @hp += amount
+    @hp = @max_hp if @hp > @max_hp
   end
 end
 
 # モンスターの設計図
 class Monster
-  attr_accessor :name, :hp
-  def initialize(name, hp)
+  attr_accessor :name, :hp, :power
+  def initialize(name, hp, power)
     @name = name
     @hp = hp
+    @power = power
   end
 
   # ダメージを受けるという「動き」も設計図に入れておく
@@ -38,7 +51,7 @@ end
 
 # --- 3. 攻撃の仕組み（メソッド） ---
 def attack(player, target, min, max)
-damage = rand(min..max)
+  damage = rand(min..max)
   target.receive_damage(damage)
   
   # 1. HPバーの長さを計算（現在のHP / 最大HP * 100）
@@ -67,7 +80,39 @@ damage = rand(min..max)
   `#{@log_area}.scrollTop = #{@log_area}.scrollHeight`
 end
 
-# --- 4. ボタンが押された時の処理 ---
+# 敵の攻撃（敵のターン）の処理
+def enemy_turn
+  damage = rand(Math.floor(@enemy.power * 0.8)..Math.floor(@enemy.power * 1.2))
+  @player.receive_damage(damage)
+  
+  # プレイヤーのHPバーを更新
+  percent = (@player.hp.to_f / @player.max_hp * 100).to_i
+  `document.getElementById('player-hp-bar').style.width = #{percent} + "%"`
+  `document.getElementById('player-hp').innerText = #{@player.hp}`
+  
+  msg = "<b>#{@enemy.name}のはんげき！#{@player.name}は#{damage}のダメージを受けた！</b><br>"
+  `#{@log_area}.innerHTML += #{msg}`
+
+  if @player.hp <= 0
+    `#{@log_area}.innerHTML += "<h2 style='color:red;'>GAME OVER...</h2>"`
+    `document.getElementById('command-menu').style.display = 'none'`
+  end
+end
+
+# こうげきボタンが押されたとき
+`document.getElementById('attack-btn').addEventListener('click', function() {`
+  # 1. プレイヤーの攻撃
+  attack(@player, @enemy, 5, 15)
+  
+  if @enemy.hp > 0
+    # 2. 少し遅れて敵のターンが来る
+    `setTimeout(function() {`
+      enemy_turn()
+    `}, 800)`
+  end
+`})`
+
+# 冒険スタートボタンが押された時の処理
 `#{@button}.addEventListener('click', function() {`
   name = `#{@player_name_input}.value`
 
@@ -80,34 +125,9 @@ end
     # ★ 設計図から実体（インスタンス）を作る！
     @player = Player.new(name)
     @enemy_count = 1
-    @enemy = Monster.new("スライム#{@enemy_count}", 30)
+    @enemy = Monster.new("スライム#{@enemy_count}", 30, 10)
     
     `#{@enemy_hp_span}.innerText = #{@enemy.hp}`
-
-    timer = `setInterval(function() {`
-      
-      # オブジェクトを渡して攻撃！
-      attack(@player, @enemy, 5, 15)
-
-      if @enemy.hp <= 0
-        finish_msg = "<b>#{@enemy.name}をたおした！</b><br>"
-        `#{@log_area}.innerHTML += #{finish_msg}`
-        
-        if @enemy_count < 3
-          @enemy_count += 1
-          # 新しいモンスターを生成して入れ替える
-          @enemy = Monster.new("スライム#{@enemy_count}", 30 + (@enemy_count * 10))
-          
-          next_msg = ">> 次の敵、#{@enemy.name}があらわれた！<br>"
-          `#{@log_area}.innerHTML += #{next_msg}`
-        else
-          `clearInterval(#{timer})`
-          `#{@log_area}.innerHTML += "<b>★すべての敵を撃破した！★</b>"`
-          `#{@log_area}.scrollTop = #{@log_area}.scrollHeight`
-          `#{@button}.disabled = false`
-        end
-      end
-    `}, 500)`
   end
 `})`
 
